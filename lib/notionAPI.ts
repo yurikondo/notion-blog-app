f3731141a671c252db68f80bc335744a9bffe358
yuri <yuri@example.com>
2023-05-30 08:34:13 +0900

    15. Notionに投稿している記事のタグを全て取得してみよう

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -21,10 +21,18 @@ export const getAllPosts = async () => {
 };
 
 const getPageMetaData = (post) => {
+  const getTags = (tags) => {
+    const allTags = tags.map((tag) => {
+      return tag.name;
+    });
+    return allTags;
+  };
+
   return {
     title: post.properties.Name.title[0].plain_text,
     description: post.properties.Description.rich_text[0].plain_text,
     date: post.properties.Date.date.start,
     // slug: post.properties.Slug.rich_text[0].plain_text,
+    tags: getTags(post.properties.Tags.multi_select),
   };
 };
